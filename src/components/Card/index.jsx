a0eb78a85bf5b7b5a9ddc2c3c7bcc8142cb156e0
margilan <shermatovelbek88@gmail.com>
2024-03-09 23:23:24 +0500

    uyga vazifa tuhatuldn

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,56 +1,50 @@
-import "./index.css";
-import { useState , useEffect } from "react";
-import React from "react";
-import img from "../../assets/carusel.png";
-import iconss from "../../assets/four.svg";
-
-
-
+import './index.css'
+import { useState , useEffect } from 'react';
+import img from '../../assets/carusel.png'
+import iconss from '../../assets/four.svg'
 
 
 function index() {
-
-     const [data , setData] = useState([]);
-    useEffect(()=>{
-        const ApiKey = 'MS644MN-J7WMZQT-Q0PDD6K-FSV5ZAA';
-    
-        fetch('https://api.kinopoisk.dev/v1.4/movie?lists=top250' , {
-            headers:{
-                "X-API-KEY" : ApiKey,
-            },
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+      const ApiKey = ' 3RZT9KG-6D14WCF-H3NJA5B-9QF9707';
+  
+      fetch('https://api.kinopoisk.dev/v1.4/movie?lists=top250', {
+        headers: {
+          "X-API-KEY": ApiKey,
+        },
+      })
+        .then((res) => res.json())
+        .then((responseData) => {
+          setData(responseData.docs);
         })
-        .then(res => res.json())
-        .then(data=>{
-            setData(data.docs)
-        })
-        .catch(err =>{
-            console.log(err);
-        })
-    } , [])
-  return (
-    <div className="wraper-container">
-    {
-        data.map((el)=>{
-            <div className="card-wrapper">
+        .catch((err) => {
+          console.log(err);
+        });
+    }, []);
+  
+    return (
+      <div className="wraper-container">
+        {data.map((movie) => (
+          <div className="card-wrapper" key={movie.id}>
             <div className="img">
               <img src={img} width={280} height={174} alt="" />
             </div>
             <span className="obwe-span">
-              <span className="iki">2019</span>
+              <span className="iki">{movie.year}</span>
               <span className="icons">
                 <span>
                   <img src={iconss} alt="icons" />
                 </span>
-                <span>{el.name}</span>
+                <span>Movie</span>
               </span>
-              <span>PG</span>
+              <span>{movie.ageRestriction}</span>
             </span>
           </div>
-        })
-    }
-      
-    </div>
-  );
-}
-
-export default index;
+        ))}
+      </div>
+    );
+  }
+  
+  export default index;
--- src/pages/Movies/index.jsx
+++ src/pages/Movies/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Card from '../../components/Card/index'
+import './index.css'
 
 function Movies() {
   return (
-    <div>Movies</div>
+    <div className='wrappper'>
+          <Card></Card>
+    </div>
   )
 }
 
